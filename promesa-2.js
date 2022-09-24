bbbff60ceef88ce34031bf288a0f095a613b06ce
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-23 18:37:42 -0600

    promesa-2 completo

--- promesa-2.js
+++ promesa-2.js
@@ -2,24 +2,26 @@
 console.log('Hi world');
 
 //funtion normal que regresa una promesa pero el reultado se obtiene mas lento
-function sumarUno(numero, resultadoCallback){
+function sumarUno(numero,){
 
     var promesa1 = new Promise (function(resolve,reject){
-      
+        if(numero>=7){
+            reject('El numero ya es muy alto');
+        }
+
         setTimeout(function(){
-            resultadoCallback(numero+1); 
+            resolve(numero+1); 
         }, 800);
     });
     return promesa1;
 }
 
 //mandar llamar a la funcion
-sumarUno(8).then(numeroNuevo=> {
-    console.log('Resultado1: ',numeroNuevo);
-    return sumarUno(numeroNuevo);
-}).then(numeroNuevo=>{
-    console.log('Resultado2: ',numeroNuevo);
-    return sumarUno(numeroNuevo);
-}).then(numeroNuevo=>{
-    console.log('Resultado3: ',numeroNuevo);
+sumarUno(8)
+.then(sumarUno)
+.then(sumarUno)
+.then(numeroNuevo=> {
+ console.log('Resultado: ',numeroNuevo);
+}).catch(error=>{
+    console.log('Error en la promesa',error)
 });
