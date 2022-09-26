f94a9d7c3de8e055d56d2beaf0a18acafb198150
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-26 15:49:16 -0600

    creamos fetch-4 inicio

--- fetch-3.js
+++ fetch-3.js
@@ -10,4 +10,10 @@ fetch('https://reqres.in/api/users',{
     headers:{
         'Content-Type':'appliction/json'
     }
-});
+})
+.then(resp => resp.json())
+.then(console.log)
+.catch(error=>{
+    console.log('Error en la peticion API POST');
+    console.log(error);
+})
