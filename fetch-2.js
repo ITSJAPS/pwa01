e138534d4e2aaa6467650c64d5f383cb839be21f
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-26 13:56:54 -0600

    fetch 2 get api completo

--- fetch-2.js
+++ fetch-2.js
@@ -2,8 +2,19 @@
 //configuracion de nuestro servicio
 //'https://reqres.in/api/users'
 
-//Creamos la funcion fetch
+//fetch original 
 fetch('https://reqres.in/api/users')
 .then(resp => {
     console.log(resp)
-})
+});
+
+//Creamos la funcion fetch con funciones anidadas declaracion ideal
+fetch('https://reqres.in/api/users')
+.then(resp => resp.json())
+.then(respObj=>{
+    console.log(respObj);
+    console.log(respObj.page);
+    console.log(respObj.per_page);
+    console.log(respObj.data);
+    
+});
