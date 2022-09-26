5ca98e8170dc3cfdfccd2377d1f962683f77cdb1
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-26 13:34:12 -0600

    fectch get api inicio

---
+++ fetch-2.js
@@ -0,0 +1,9 @@
+
+//configuracion de nuestro servicio
+//'https://reqres.in/api/users'
+
+//Creamos la funcion fetch
+fetch('https://reqres.in/api/users')
+.then(resp => {
+    console.log(resp)
+})
