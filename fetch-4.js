111b63aa49ea65947bbb48168475a57055bd4cbf
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-26 21:50:27 -0600

    fetch-4 completo

--- fetch-4.js
+++ fetch-4.js
@@ -1,3 +1,28 @@
+//intanciamos lo dos componentes de imagen de la paguina index.html
+let img1= document.getElementById("IdImg1");
+let img2= document.getElementById("IdImg2");
+
+
+
 //Creamos un fetch con codigo de promesa pero simplificada
 fetch('imagenes/MazingerZ-1.png')
-.then(resp=> )
+.then(resp=> resp.blob())
+.then(image =>{
+    console.log(image);
+    
+    //creamos instancia para imagen primera
+    var imgPath1 = URL.createObjectURL(image);
+    img1.src=imgPath1;
+});
+//fetch con codigo de promesa normal 
+fetch('imagenes/MazingerZ-2.png')
+.then(resp =>{
+    return resp.blob();
+})
+.then(image=>{
+    console.log(image);
+    
+    //creamos la segunda instancia imagen 
+    var imgPath2 =URL.createObjectURL(image);
+    img2.src = imgPath2;
+});
